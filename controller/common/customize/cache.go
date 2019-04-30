32047ba3613da6f2ab714aa967b495d9f4c775b5
Sven Bartscher <sven.bartscher@credativ.de>
2019-04-30 12:13:26 +0200

    refactor: Rename related and customize components
    The components for handling related objects and the customize hook
    have previously been named either something referring to either
    related or customize. But the current implementation doesn't actually
    have a clear line between the two concepts and makes the mmostly
    inseparable, so they are moved to a common namespace with consistent
    naming.
    
    Signed-off-by: grzesuav <grzesuav@gmail.com>

--- controller/common/customize/cache.go
+++ controller/common/customize/cache.go
@@ -1,24 +1,20 @@
-package related
-
-import (
-	"metacontroller.app/controller/common"
-)
+package customize
 
 type CustomizeResponseCache map[string]customizeResponseCacheEntry
 
 type customizeResponseCacheEntry struct {
 	parentGeneration int64
-	cachedResponse   *common.CustomizeHookResponse
+	cachedResponse   *CustomizeHookResponse
 }
 
-func (crc CustomizeResponseCache) Add(name string, parentGeneration int64, response *common.CustomizeHookResponse) {
+func (crc CustomizeResponseCache) Add(name string, parentGeneration int64, response *CustomizeHookResponse) {
 	crc[name] = customizeResponseCacheEntry{
 		parentGeneration: parentGeneration,
 		cachedResponse:   response,
 	}
 }
 
-func (crc CustomizeResponseCache) Get(name string, parentGeneration int64) *common.CustomizeHookResponse {
+func (crc CustomizeResponseCache) Get(name string, parentGeneration int64) *CustomizeHookResponse {
 	cacheEntry, ok := crc[name]
 	if !ok || cacheEntry.parentGeneration != parentGeneration {
 		return nil
