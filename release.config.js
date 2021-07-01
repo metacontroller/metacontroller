381e30c3d12147b04a670846cdf0921475bb083a
Mathieu Parent <mathieu.parent@insee.fr>
2021-07-01 20:42:19 +0200

    chore: #42 - Add helm chart
    Fixes: #42
    Signed-off-by: Mathieu Parent <mathieu.parent@insee.fr>

--- release.config.js
+++ release.config.js
@@ -26,6 +26,20 @@ module.exports = {
               }
             ],
             "countMatches": true
+          },
+          {
+            "files": ["deploy/helm/metacontroller/Chart.yaml"],
+            "from": "^(version|appVersion): v.*",
+            "to": "$1: v${nextRelease.version}",
+            "results": [
+              {
+                "file": "deploy/helm/metacontroller/Chart.yaml",
+                "hasChanged": true,
+                "numMatches": 2,
+                "numReplacements": 2
+              }
+            ],
+            "countMatches": true
           }
         ]
       }
@@ -33,12 +47,12 @@ module.exports = {
     [
       "@semantic-release/git",
       {
-        "assets": ["CHANGELOG.md", "manifests/production/metacontroller.yaml"],
+        "assets": ["CHANGELOG.md", "manifests/production/metacontroller.yaml", "deploy/helm/metacontroller/Chart.yaml"],
         "message": "chore(release): ${nextRelease.version}\n\n${nextRelease.notes}"
       }
     ],
     [
-      '@semantic-release/github', 
+      '@semantic-release/github',
       {
         "assets": ["manifests/production/*"]
       }
