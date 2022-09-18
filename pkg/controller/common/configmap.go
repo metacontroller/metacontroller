8e41414005f949cdd75937d01455084f7e0937e8
Petr Stukalov <petr@polkapad.network>
2022-09-18 10:57:28 +0300

    composition controller, remove configmap if default update strategy is OnDelete

--- pkg/controller/common/configmap.go
+++ pkg/controller/common/configmap.go
@@ -16,7 +16,7 @@ func (m inPlace) GetMethod(string, string) v1alpha1.ChildUpdateMethod {
 	return v1alpha1.ChildUpdateInPlace
 }
 
-func GetGvkFromConfigMap(
+func GetGvkFromConfigMapCC(
 	dynClient *dynamicclientset.Clientset,
 	parent *unstructured.Unstructured,
 ) ([]commonv1.GroupVersionKind, error) {
@@ -33,9 +33,9 @@ func GetGvkFromConfigMap(
 	}
 
 	data := configMap.Object["data"].(map[string]interface{})
-	resources := data["resources"].(string)
+	children := data["children"].(string)
 
-	err = json.Unmarshal([]byte(resources), &result)
+	err = json.Unmarshal([]byte(children), &result)
 	if err != nil {
 		return result, err
 	}
@@ -43,77 +43,96 @@ func GetGvkFromConfigMap(
 	return result, err
 }
 
-func UpdateConfigMap(
+func UpdateConfigMapCC(
+	defaultUpdateStrategy v1alpha1.ChildUpdateMethod,
 	dynClient *dynamicclientset.Clientset,
 	parent *unstructured.Unstructured,
 	desiredChildren commonv1.RelativeObjectMap,
 ) ([]commonv1.GroupVersionKind, error) {
 	var result []commonv1.GroupVersionKind
-
 	client, err := dynClient.Kind("v1", "ConfigMap")
 	if err != nil {
 		return result, err
 	}
 
-	var gvks = map[commonv1.GroupVersionKind]bool{}
-	for key := range desiredChildren {
-		gvks[key] = false
-	}
+	if defaultUpdateStrategy != v1alpha1.ChildUpdateOnDelete {
 
-	var observedChildren commonv1.RelativeObjectMap
-	configMap, err := client.Namespace(parent.GetNamespace()).Get(context.TODO(), string(parent.GetUID()), metav1.GetOptions{})
-	if err == nil {
-		data := configMap.Object["data"].(map[string]interface{})
-		resources := data["resources"].(string)
+		var gvks = map[commonv1.GroupVersionKind]bool{}
+		for key := range desiredChildren {
+			gvks[key] = false
+		}
 
-		var arr []commonv1.GroupVersionKind
-		err = json.Unmarshal([]byte(resources), &arr)
+		var observedChildren commonv1.RelativeObjectMap
+		configMap, err := client.Namespace(parent.GetNamespace()).Get(context.TODO(), string(parent.GetUID()), metav1.GetOptions{})
+		if err == nil {
+			data := configMap.Object["data"].(map[string]interface{})
+			children := data["children"].(string)
+
+			var arr []commonv1.GroupVersionKind
+			err = json.Unmarshal([]byte(children), &arr)
+			if err != nil {
+				return result, err
+			}
+			for _, gvk := range arr {
+				gvks[gvk] = false
+			}
+
+			observedChildren = commonv1.MakeRelativeObjectMap(
+				configMap,
+				[]*unstructured.Unstructured{configMap},
+			)
+		} else {
+			observedChildren = commonv1.RelativeObjectMap{}
+		}
+
+		configMap = &unstructured.Unstructured{
+			Object: map[string]interface{}{
+				"apiVersion": "v1",
+				"kind":       "ConfigMap",
+				"metadata": map[string]interface{}{
+					"name": parent.GetUID(),
+				},
+				"data": map[string]interface{}{},
+			},
+		}
+
+		for key := range gvks {
+			result = append(result, key)
+		}
+		j, err := json.Marshal(result)
 		if err != nil {
 			return result, err
 		}
-		for _, gvk := range arr {
-			gvks[gvk] = false
+		res := string(j)
+		configMap.Object["data"] = map[string]interface{}{
+			"children": res,
 		}
 
+		var gvk = commonv1.GroupVersionKind{configMap.GroupVersionKind()}
+		var objects = map[string]*unstructured.Unstructured{
+			configMap.GetName(): configMap,
+		}
+
+		if err := updateChildren(client, inPlace{}, parent, observedChildren[gvk], objects); err != nil {
+			return result, err
+		}
+
+		return result, nil
+	} else {
+		configMap, err := client.Namespace(parent.GetNamespace()).Get(context.TODO(), string(parent.GetUID()), metav1.GetOptions{})
+		if err != nil {
+			return result, nil
+		}
+		var observedChildren commonv1.RelativeObjectMap
 		observedChildren = commonv1.MakeRelativeObjectMap(
 			configMap,
 			[]*unstructured.Unstructured{configMap},
 		)
-	} else {
-		observedChildren = commonv1.RelativeObjectMap{}
+		var objects = map[string]*unstructured.Unstructured{}
+		var gvk = commonv1.GroupVersionKind{configMap.GroupVersionKind()}
+		if err := deleteChildren(client, parent, observedChildren[gvk], objects); err != nil {
+			return result, err
+		}
+		return result, nil
 	}
-
-	configMap = &unstructured.Unstructured{
-		Object: map[string]interface{}{
-			"apiVersion": "v1",
-			"kind":       "ConfigMap",
-			"metadata": map[string]interface{}{
-				"name": parent.GetUID(),
-			},
-			"data": map[string]interface{}{},
-		},
-	}
-
-	for key := range gvks {
-		result = append(result, key)
-	}
-	j, err := json.Marshal(result)
-	if err != nil {
-		return result, err
-	}
-	res := string(j)
-	configMap.Object["data"] = map[string]interface{}{
-		"resources": res,
-	}
-
-	var gvk = commonv1.GroupVersionKind{configMap.GroupVersionKind()}
-	var objects = map[string]*unstructured.Unstructured{
-		configMap.GetName(): configMap,
-	}
-
-	if err := updateChildren(client, inPlace{}, parent, observedChildren[gvk], objects); err != nil {
-		return result, err
-	}
-
-	return result, nil
 }
