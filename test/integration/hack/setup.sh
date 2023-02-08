372edd6602410011b567763e9160d4c3b55480a6
Matthew Gamble <matthew.gamble@rea-group.com>
2023-02-09 00:36:40 +1100

    fix: Support testing on arm64
    gotestsum provides pre-compiled binaries for arm64, but the script was
    previously hard-coded to only download amd64 binaries. Now it will
    detect the correct architecture to use before downloading.
    
    I also took this opportunity to surround an instance of ${PWD} in
    quotes, so that it's safer and more consistent with the rest of the
    script.
    
    Signed-off-by: Matthew Gamble <matthew.gamble@rea-group.com>

--- test/integration/hack/setup.sh
+++ test/integration/hack/setup.sh
@@ -14,7 +14,16 @@ else
     echo ""
     echo "+++ downloading gotestsum"
     echo ""
+    uname_arch="$(uname -m)"
+    if [[ "${uname_arch}" == "x86_64" ]] ; then
+        gotestsum_arch="amd64"
+    elif [[ "${uname_arch}" == "aarch64" ]] ; then
+        gotestsum_arch="arm64"
+    else
+        >&2 echo "Unknown CPU architecture, cannot install gotestsum"
+        exit 1
+    fi
     mkdir -p "${PWD}"/bin/
-    curl -sSL "https://github.com/gotestyourself/gotestsum/releases/download/v0.6.0/gotestsum_0.6.0_linux_amd64.tar.gz" | tar -xz -C ${PWD}/bin gotestsum
+    curl -sSL "https://github.com/gotestyourself/gotestsum/releases/download/v0.6.0/gotestsum_0.6.0_linux_${gotestsum_arch}.tar.gz" | tar -xz -C "${PWD}"/bin gotestsum
     chmod +x "${PWD}"/bin/gotestsum
 fi
