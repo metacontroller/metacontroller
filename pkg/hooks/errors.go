b1498334321fce1c370acc91321c759b62edbc73
Weijian Yang <weijian.yang@rea-group.com>
2023-03-06 13:41:21 +0800

    feat(handle-429): #533 - Support rate limiting based on retry-after header.
    Signed-off-by: Weijian Yang <weijian.yang@rea-group.com>

---
+++ pkg/hooks/errors.go
@@ -0,0 +1,11 @@
+package hooks
+
+import "fmt"
+
+type TooManyRequestError struct {
+	AfterSecond int
+}
+
+func (e *TooManyRequestError) Error() string {
+	return fmt.Sprintf("Too many request, it will be resync after: %d", e.AfterSecond)
+}
