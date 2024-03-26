439b219f096a3198a875ea8ee4e0a0dc1fefe812
Craig Newton <newtondev@gmail.com>
2024-03-26 10:10:14 +0100

    chore(fix-deps): update code generator version to 0.29.3; ran codegen to update generated files
    Signed-off-by: Craig Newton <newtondev@gmail.com>

--- pkg/client/generated/clientset/internalclientset/clientset.go
+++ pkg/client/generated/clientset/internalclientset/clientset.go
@@ -32,8 +32,7 @@ type Interface interface {
 	MetacontrollerV1alpha1() metacontrollerv1alpha1.MetacontrollerV1alpha1Interface
 }
 
-// Clientset contains the clients for groups. Each group has exactly one
-// version included in a Clientset.
+// Clientset contains the clients for groups.
 type Clientset struct {
 	*discovery.DiscoveryClient
 	metacontrollerV1alpha1 *metacontrollerv1alpha1.MetacontrollerV1alpha1Client
