6f3bea202c7d3f9a2fdc8285e6b5bebe259613ce
ajayk <ajaykemparaj@gmail.com>
2024-05-21 13:59:25 -0700

    golreaser: remove deprecated flags
    Signed-off-by: ajayk <ajaykemparaj@gmail.com>

--- .release.sh
+++ .release.sh
@@ -2,4 +2,4 @@
 set -euxo pipefail
 
 printf "%s" "$1" > /tmp/release-notes.md
-goreleaser release --release-notes /tmp/release-notes.md --rm-dist
+goreleaser release --release-notes /tmp/release-notes.md --clean
