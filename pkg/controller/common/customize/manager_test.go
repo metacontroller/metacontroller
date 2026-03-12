7fcb607f9e4b8ded1a5f15ec12dd3bb109d71315
grzesuav <grzesuav@gmail.com>
2026-03-12 23:32:10 +0100

    chore: Address issue with namespace selector for cluster scope resources
    Signed-off-by: grzesuav <grzesuav@gmail.com>

--- pkg/controller/common/customize/manager_test.go
+++ pkg/controller/common/customize/manager_test.go
@@ -17,16 +17,20 @@ limitations under the License.
 package customize
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	v1 "metacontroller/pkg/controller/common/customize/api/v1"
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/go-logr/logr/funcr"
 
 	"metacontroller/pkg/apis/metacontroller/v1alpha1"
 	"metacontroller/pkg/controller/common"
 	dynamicclientset "metacontroller/pkg/dynamic/clientset"
+	dynamicdiscovery "metacontroller/pkg/dynamic/discovery"
 	dynamicinformer "metacontroller/pkg/dynamic/informer"
 
 	"metacontroller/pkg/internal/testutils/dynamic/discovery"
@@ -34,7 +38,11 @@ import (
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
-	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/dynamic/fake"
+	fakeclientset "k8s.io/client-go/kubernetes/fake"
 	"k8s.io/client-go/rest"
 )
 
@@ -652,9 +660,32 @@ func Test_matchRelatedRule(t *testing.T) {
 			wantMatch:       false,
 			wantErr:         true,
 			dynInformers: dynamicinformer.NewSharedInformerFactory(
-				dynamicclientset.NewClientset(&rest.Config{}, discovery.NewFakeResourceMap(fake.NewSimpleClientset()), nil),
+				dynamicclientset.NewClientset(&rest.Config{}, discovery.NewFakeResourceMap(fakeclientset.NewClientset()), nil),
 				0,
 			),
+		},
+		{
+			name:         "return error when namespaceSelector is used for cluster-scoped resource",
+			hookVersion:  v1alpha1.HookVersionV2,
+			isNamespaced: false,
+			parent:       fakeGenericParentWithNamespace(),
+			related: func() *unstructured.Unstructured {
+				rc := &unstructured.Unstructured{}
+				rc.SetAPIVersion("v1")
+				rc.SetKind("ClusterRole")
+				rc.SetName("name")
+				return rc
+			}(),
+			relatedRule: &v1alpha1.RelatedResourceRule{
+				ResourceRule: v1alpha1.ResourceRule{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Resource:   "clusterroles",
+				},
+				NamespaceSelector: &metav1.LabelSelector{MatchLabels: map[string]string{"env": "prod"}},
+			},
+			relatedRuleKind: "ClusterRole",
+			wantMatch:       false,
+			wantErr:         true,
 		},
 	}
 
@@ -672,7 +703,7 @@ func Test_matchRelatedRule(t *testing.T) {
 				dynInformers: tt.dynInformers,
 				nsInformer:   nsInformer,
 			}
-			matches, err := rm.matchesRelatedRule(tt.hookVersion, tt.isNamespaced, tt.parent, tt.related, tt.relatedRule, tt.relatedRuleKind)
+			matches, err := rm.matchesRelatedRule(tt.hookVersion, tt.isNamespaced, tt.parent, tt.related, tt.relatedRule, tt.relatedRuleKind, tt.related.GetNamespace() != "")
 			if err != nil && !tt.wantErr {
 				t.Error(err)
 			}
@@ -680,5 +711,271 @@ func Test_matchRelatedRule(t *testing.T) {
 				t.Errorf("Expected match: %v, actual match: %v", tt.wantMatch, matches)
 			}
 		})
+	}
+}
+
+func TestGetRelatedObjects_ErrorWhenNamespaceSelectorForClusterScopedResource(t *testing.T) {
+	// Setup fake discovery
+	simple := fakeclientset.NewClientset()
+	fakeDiscovery := simple.Discovery().(*fakediscovery.FakeDiscovery)
+	fakeDiscovery.Resources = []*metav1.APIResourceList{
+		{
+			GroupVersion: "rbac.authorization.k8s.io/v1",
+			APIResources: []metav1.APIResource{
+				{
+					Name:       "clusterroles",
+					Kind:       "ClusterRole",
+					Namespaced: false,
+					Group:      "rbac.authorization.k8s.io",
+					Version:    "v1",
+				},
+			},
+		},
+		{
+			GroupVersion: "v1",
+			APIResources: []metav1.APIResource{
+				{
+					Name:       "namespaces",
+					Kind:       "Namespace",
+					Namespaced: false,
+					Group:      "",
+					Version:    "v1",
+				},
+			},
+		},
+	}
+	resourceMap := discovery.NewFakeResourceMap(simple)
+
+	// Setup fake dynamic client
+	scheme := runtime.NewScheme()
+	fakeDynClient := fake.NewSimpleDynamicClientWithCustomListKinds(scheme, map[schema.GroupVersionResource]string{
+		{Group: "rbac.authorization.k8s.io", Version: "v1", Resource: "clusterroles"}: "ClusterRoleList",
+		{Group: "", Version: "v1", Resource: "namespaces"}:                            "NamespaceList",
+	})
+	dynClient := dynamicclientset.NewClientset(&rest.Config{}, resourceMap, fakeDynClient)
+
+	// Setup informers
+	dynInformers := dynamicinformer.NewSharedInformerFactory(dynClient, 0)
+	nsInformer, _ := dynInformers.Resource("v1", "namespaces")
+	defer nsInformer.Close()
+
+	// Start informers and wait for sync
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if !waitForSync(ctx, nsInformer) {
+		t.Fatal("Timed out waiting for namespace informer sync")
+	}
+
+	// Create manager
+	parentGK := schema.GroupKind{Group: "test", Kind: "Parent"}
+	parentResource := &dynamicdiscovery.APIResource{
+		APIResource: metav1.APIResource{Name: "parents", Namespaced: true, Kind: "Parent", Group: "test", Version: "v1"},
+	}
+	parentKinds := common.NewGroupKindMap()
+	parentKinds.Set(parentGK, parentResource)
+
+	rm := &Manager{
+		controller:       &v1alpha1.CompositeController{},
+		parentKinds:      parentKinds,
+		dynClient:        dynClient,
+		dynInformers:     dynInformers,
+		nsInformer:       nsInformer,
+		parentInformers:  common.NewInformerMap(),
+		relatedInformers: common.NewInformerMap(),
+		logger:           fakeLogger,
+		stopCh:           stopCh,
+		customizeCache:   newResponseCache(),
+	}
+
+	// Setup customize hook response
+	expectedResponse := &v1.CustomizeHookResponse{
+		Version: v1alpha1.HookVersionV2,
+		RelatedResourceRules: []*v1alpha1.RelatedResourceRule{
+			{
+				ResourceRule: v1alpha1.ResourceRule{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Resource:   "clusterroles",
+				},
+				NamespaceSelector: &metav1.LabelSelector{MatchLabels: map[string]string{"env": "prod"}},
+			},
+		},
+	}
+	rm.customizeHook = NewHookExecutorStub(expectedResponse)
+
+	// Trigger creation and wait for sync of the related informer
+	for {
+		_, informer, err := rm.getRelatedClient("rbac.authorization.k8s.io/v1", "clusterroles")
+		if err == nil {
+			if informer.Informer().HasSynced() {
+				break
+			}
+		} else if !errors.Is(err, ErrRelatedInformerNotSynced) {
+			t.Fatalf("Failed to get related client: %v", err)
+		}
+		time.Sleep(10 * time.Millisecond)
+		select {
+		case <-ctx.Done():
+			t.Fatal("Timed out waiting for related informer sync")
+		default:
+		}
+	}
+
+	// Create parent object
+	parent := &unstructured.Unstructured{}
+	parent.SetAPIVersion("test/v1")
+	parent.SetKind("Parent")
+	parent.SetName("test-parent")
+	parent.SetNamespace("test-ns")
+	parent.SetUID("123")
+	parent.SetGeneration(1)
+
+	// Call GetRelatedObjects
+	_, err := rm.GetRelatedObjects(parent)
+
+	if err == nil {
+		t.Fatal("Expected error when using namespaceSelector for cluster-scoped resource, but got nil")
+	}
+
+	expectedErr := "namespaceSelector is only supported for namespaced related resources"
+	if err.Error() != expectedErr {
+		t.Errorf("Expected error %q, got %q", expectedErr, err.Error())
+	}
+}
+
+func TestGetRelatedObjects_IgnoreNamespaceForClusterScopedResource(t *testing.T) {
+	// Setup fake discovery
+	simple := fakeclientset.NewClientset()
+	fakeDiscovery := simple.Discovery().(*fakediscovery.FakeDiscovery)
+	fakeDiscovery.Resources = []*metav1.APIResourceList{
+		{
+			GroupVersion: "rbac.authorization.k8s.io/v1",
+			APIResources: []metav1.APIResource{
+				{
+					Name:       "clusterroles",
+					Kind:       "ClusterRole",
+					Namespaced: false,
+					Group:      "rbac.authorization.k8s.io",
+					Version:    "v1",
+					Verbs:      []string{"get", "list", "watch"},
+				},
+			},
+		},
+	}
+	resourceMap := discovery.NewFakeResourceMap(simple)
+
+	// Setup fake dynamic client with some objects
+	scheme := runtime.NewScheme()
+	clusterRole := &unstructured.Unstructured{}
+	clusterRole.SetAPIVersion("rbac.authorization.k8s.io/v1")
+	clusterRole.SetKind("ClusterRole")
+	clusterRole.SetName("test-clusterrole")
+	clusterRole.SetLabels(map[string]string{"app": "test"})
+
+	fakeDynClient := fake.NewSimpleDynamicClientWithCustomListKinds(scheme, map[schema.GroupVersionResource]string{
+		{Group: "rbac.authorization.k8s.io", Version: "v1", Resource: "clusterroles"}: "ClusterRoleList",
+	}, clusterRole)
+	dynClient := dynamicclientset.NewClientset(&rest.Config{}, resourceMap, fakeDynClient)
+
+	// Setup informers
+	dynInformers := dynamicinformer.NewSharedInformerFactory(dynClient, 0)
+
+	// Start informers and wait for sync
+	stopCh := make(chan struct{})
+	defer close(stopCh)
+
+	// Create manager
+	parentGK := schema.GroupKind{Group: "test", Kind: "Parent"}
+	parentResource := &dynamicdiscovery.APIResource{
+		APIResource: metav1.APIResource{Name: "parents", Namespaced: true, Kind: "Parent", Group: "test", Version: "v1"},
+	}
+	parentKinds := common.NewGroupKindMap()
+	parentKinds.Set(parentGK, parentResource)
+
+	rm := &Manager{
+		controller:       &v1alpha1.CompositeController{},
+		parentKinds:      parentKinds,
+		dynClient:        dynClient,
+		dynInformers:     dynInformers,
+		parentInformers:  common.NewInformerMap(),
+		relatedInformers: common.NewInformerMap(),
+		logger:           fakeLogger,
+		stopCh:           stopCh,
+		customizeCache:   newResponseCache(),
+	}
+
+	// Setup customize hook response with explicit namespace for cluster-scoped resource
+	expectedResponse := &v1.CustomizeHookResponse{
+		Version: v1alpha1.HookVersionV2,
+		RelatedResourceRules: []*v1alpha1.RelatedResourceRule{
+			{
+				ResourceRule: v1alpha1.ResourceRule{
+					APIVersion: "rbac.authorization.k8s.io/v1",
+					Resource:   "clusterroles",
+				},
+				Namespace:     "some-namespace", // This should be ignored
+				LabelSelector: &metav1.LabelSelector{MatchLabels: map[string]string{"app": "test"}},
+			},
+		},
+	}
+	rm.customizeHook = NewHookExecutorStub(expectedResponse)
+
+	// Trigger creation and wait for sync of the related informer
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	for {
+		_, informer, err := rm.getRelatedClient("rbac.authorization.k8s.io/v1", "clusterroles")
+		if err == nil {
+			if informer.Informer().HasSynced() {
+				break
+			}
+		} else if !errors.Is(err, ErrRelatedInformerNotSynced) {
+			t.Fatalf("Failed to get related client: %v", err)
+		}
+		time.Sleep(10 * time.Millisecond)
+		select {
+		case <-ctx.Done():
+			t.Fatal("Timed out waiting for related informer sync")
+		default:
+		}
+	}
+
+	// Create parent object
+	parent := &unstructured.Unstructured{}
+	parent.SetAPIVersion("test/v1")
+	parent.SetKind("Parent")
+	parent.SetName("test-parent")
+	parent.SetNamespace("test-ns")
+	parent.SetUID("123")
+	parent.SetGeneration(1)
+
+	// Call GetRelatedObjects
+	relatedObjects, err := rm.GetRelatedObjects(parent)
+
+	if err != nil {
+		t.Fatalf("Expected no error, but got: %v", err)
+	}
+
+	// Verify that the clusterrole was found despite the namespace being specified
+	list := relatedObjects.List()
+	if len(list) != 1 {
+		t.Errorf("Expected 1 related object, got %d", len(list))
+	} else if list[0].GetName() != "test-clusterrole" {
+		t.Errorf("Expected test-clusterrole, got %s", list[0].GetName())
+	}
+}
+
+func waitForSync(ctx context.Context, informer *dynamicinformer.ResourceInformer) bool {
+	for {
+		select {
+		case <-ctx.Done():
+			return false
+		case <-time.After(100 * time.Millisecond):
+			if informer.Informer().HasSynced() {
+				return true
+			}
+		}
 	}
 }
